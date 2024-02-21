55760759d9d09372ccf3c335614650faf6eecc70
Akash Desai <137322203+Akash0o7@users.noreply.github.com>
2024-02-21 22:08:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import bodyparser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (req.body.password === "iLoveProgramming") {
+    res.sendFile(__dirname + "/public/secret.html");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Connected to server port 3000");
+});
